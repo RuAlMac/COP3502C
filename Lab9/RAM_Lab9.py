49cf36c1ce04c5502b2d872f3c2658a053561bc8
Robert Giasson <79480983+rmgiasson@users.noreply.github.com>
2024-04-10 08:42:28 -0400

    Update RAM_Lab9.py

--- Lab9/RAM_Lab9.py
+++ Lab9/RAM_Lab9.py
@@ -6,30 +6,23 @@ def encode(string):
 
     for i in range(len(inList)):
         inList[i] = int(inList[i])
-        inList[i] = inList[i] + 3
-
-        if inList[i] >= 10:
-            inList[i] = inList[i] - 10
-
+        inList[i] = (inList[i] + 3) % 10
         inList[i] = str(inList[i])
 
     outString = ''.join(inList)
     return outString
+
 
 def decode(string):
-    inList = list(string)
+    num = str(string)
+    code = 0
+    counter = 1
+    for i in range(len(num) - 1, -1, -1):
+        code += ((int(num[i]) + 7) % 10) * counter
+        counter *= 10
 
-    for i in range(len(inList)):
-        inList[i] = int(inList[i])
-        inList[i] = inList[i] - 3
+    return str(code)
 
-        if inList[i] < 0:
-            inList[i] = inList[i] + 10
-
-        inList[i] = str(inList[i])
-
-    outString = ''.join(inList)
-    return outString
 
 def printMenu():
     print("Menu")
@@ -42,9 +35,10 @@ def printMenu():
     option = int(input("Please enter an option: "))
     return option
 
+
 if __name__ == '__main__':
     encoded = 0
-    while(1):
+    while (1):
         option = printMenu()
 
         match option:
@@ -52,6 +46,7 @@ if __name__ == '__main__':
                 toEncode = input("Please enter your password to encode: ")
                 encoded = encode(toEncode)
                 print("Your password has been encoded and stored!")
+                print(encoded)
             case 2:
                 decoded = decode(encoded)
                 print(f"The encoded password is {encoded}, and the original password is {decoded}.")
