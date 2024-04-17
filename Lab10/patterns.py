dc7fc2e5cb17aa779f843d9788c39ef208d92810
Russell MacGregor <110684528+RuAlMac@users.noreply.github.com>
2024-04-17 09:30:44 -0400

    Updated Lab 10

--- Lab10/patterns.py
+++ Lab10/patterns.py
@@ -1,5 +1,3 @@
-# Lab 10
-
 def gradient(n, file=None):
     for i in range(n):
         for j in range(n):
@@ -21,11 +19,3 @@ def circle(n, file=None):
             print(max(int(distance - n ** 0.5), 0),
                   end=" ", file=file)
         print(file=file)
-
-
-def make_image(pattern, size, filename):
-        with open(filename, "w") as file:
-            file.write(f"P2 {size} {size} {size}\n")
-            pattern(size, file)
-
-make_image(square, 5, "square.pgm")
